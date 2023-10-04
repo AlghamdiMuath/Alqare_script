6b88a11c440b63a6de7760b8356af35934727e96
AlghamdiMuath <muathalghamdy@gmail.com>
2023-10-04 13:59:18 +0300

    Committing lines of Alqare_script.py

--- Alqare_script.py
+++ Alqare_script.py
@@ -8,3 +8,13 @@ get_ipython().system('pip install requests beautifulsoup4')
 
 
 # In[29]:
+
+
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# # Define the website URL
+# url = "https://arbml.github.io/ARBML/Interfaces/Website/ArabicTextDetection/index.html"
+

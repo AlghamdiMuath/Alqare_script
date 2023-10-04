20a4f088bd683b798b66684fa896987cb1720c1c
AlghamdiMuath <muathalghamdy@gmail.com>
2023-10-04 13:59:24 +0300

    Committing lines of Alqare_script.py

--- Alqare_script.py
+++ Alqare_script.py
@@ -18,3 +18,13 @@ from selenium.webdriver.common.by import By
 # # Define the website URL
 # url = "https://arbml.github.io/ARBML/Interfaces/Website/ArabicTextDetection/index.html"
 
+# # Define the folder path containing the images
+# image_folder = "C:/Users/muath/Downloads/OnePage_alqare"
+
+# # Initialize the Chrome WebDriver
+# driver = webdriver.Chrome()
+
+# # Navigate to the website
+# driver.get(url)
+
+# # Wait for the page to load (you may need to adjust the sleep time depending on the website's loading time)
